cf4cf21e4508aa1cacd0569b6244c1ad0328ff8a
David McAfee <me@david-mcafee.com>
2016-11-01 11:41:30 -0700

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!("users")
+User.create!()
