26e903cd0ea39b91def6cfab190ec53e96fbdf7a
David McAfee <me@david-mcafee.com>
2016-11-01 11:33:06 -0700

    create db

---
+++ db/migrate/20161101181747_create_tracks.rb
@@ -0,0 +1,14 @@
+class CreateTracks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tracks do |t|
+      t.string :title, null: false
+      t.string :image_url, null: false
+      t.string :audio_url, null: false
+      t.integer :artist_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :tracks, :title
+  end
+end
