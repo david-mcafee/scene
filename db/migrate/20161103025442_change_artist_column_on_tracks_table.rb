1a13e78886bfb716e7b79df4f242d8ca9046c29b
David McAfee <me@david-mcafee.com>
2016-11-02 20:40:54 -0700

    update tracks table

---
+++ db/migrate/20161103025442_change_artist_column_on_tracks_table.rb
@@ -0,0 +1,5 @@
+class ChangeArtistColumnOnTracksTable < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :tracks, :artist_id, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102224225) do
+ActiveRecord::Schema.define(version: 20161103025442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20161102224225) do
     t.string   "title",      null: false
     t.string   "image_url"
     t.string   "audio_url",  null: false
-    t.integer  "artist_id",  null: false
+    t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_tracks_on_title", using: :btree
