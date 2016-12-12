1d98c84f31cf250c282fcc2f8373b3f0d5efd05b
David McAfee <me@david-mcafee.com>
2016-12-12 09:48:32 -0800

    fix initial song load formatting

--- frontend/components/player/player_container.jsx
+++ frontend/components/player/player_container.jsx
@@ -17,25 +17,26 @@ const mapStateToProps = (state) => {
 
   return(
     {
+      current_track_username: state.player.current_track_username || "The Undercover Dream Lovers",
       current_track: state.player.current_track || {
-        "id": 1,
-        "title": "Guest Song (Please Edit or Delete!)",
-        "image_url": "http://res.cloudinary.com/localscene/image/upload/v1478806147/xamd8t29ogoptv4yo0vp.png",
+        "id": 45,
+        "title": "When You Know It's Alright",
+        "image_url": "http://res.cloudinary.com/localscene/image/upload/v1478765152/cover_qty27o.jpg",
         "audio_url": "http://res.cloudinary.com/localscene/video/upload/v1478765314/The_Undercover_Dream_Lovers_-_While_It_s_In_Style_-_01_When_You_Know_It_s_Alright_o0waqh.mp3",
-        "user_id": 1,
+        "user_id": 5,
         "user": {
-        "id": 1,
-        "username": "Guest",
-        "email": "guest@david-mcafee.com",
+        "id": 5,
+        "username": "The Undercover Dream Lovers",
+        "email": "theundercoverdreamlovers@david-mcafee.com",
         "artist": "t",
-        "banner_url": "https://s-media-cache-ak0.pinimg.com/236x/a7/7e/3d/a77e3d34eb1a05f7bc930d3f719c0846.jpg",
-        "password_digest": "$2a$10$g53l95ywDIj4mrE2CJbW1eajoijMlE2ICdNvd95XIELozvNPPUzZ2",
-        "session_token": "kjYblpNeNUAfRNGOwaSMAw==",
-        "created_at": "2016-12-11T23:37:01.451Z",
-        "updated_at": "2016-12-12T01:26:05.313Z"
+        "banner_url": "http://res.cloudinary.com/localscene/image/upload/c_crop,h_960,w_960/v1478765153/UndercoverDreamLoversProfile_bmc70g.jpg",
+        "password_digest": "$2a$10$Aw58fkn2xuQr27rTegCpbeKUksSgpMVpKc.Cemc2v0IQV3ZKP/TXu",
+        "session_token": "KrnkgNa4e67Oa4ZPGMLU+A==",
+        "created_at": "2016-12-11T23:37:01.795Z",
+        "updated_at": "2016-12-11T23:37:01.795Z"
         },
         "comments": [],
-        "age": "about 2 hours"
+        "age": "about 18 hours"
         },
       currentTrackUrl: currentTrackUrl,
       playing: state.player.playing
