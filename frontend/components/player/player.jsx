b75d5eeb1c30aaf431c2d28f0da6e3290c2ded24
David McAfee <me@david-mcafee.com>
2016-12-12 09:41:45 -0800

    update player render method to display current_track's username field

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -182,7 +182,7 @@ class Player extends React.Component {
           }
           <div>
             <p className="player-track-name">{this.props.current_track ? this.props.current_track.title : ""}</p>
-            <p className="player-artist-name">{}</p>
+            <p className="player-artist-name">{this.props.current_track ? this.props.current_track.current_track_username : ""}</p>
 
           </div>
         </section>
